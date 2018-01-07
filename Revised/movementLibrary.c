f8ff85432ab7569be21bd5ab8e3824d6eec2ad95
Izapicaro <34921842+Izapicaro@users.noreply.github.com>
2018-01-07 20:47:22 +0000

    Update movementLibrary.c

--- Revised/movementLibrary.c
+++ Revised/movementLibrary.c
@@ -16,8 +16,8 @@ struct Robot {
 void go(Robot robot, int lPower, int rPower, int sPower){
 	motor[robot.left] = lPower;
 	motor[robot.left2] = lPower;
-	motor[robot.right] = rPower;
-	motor[robot.right2] = rPower;
+	motor[robot.right] = -rPower;
+	motor[robot.right2] = -rPower;
 	motor[robot.side] = sPower;
 }
 
