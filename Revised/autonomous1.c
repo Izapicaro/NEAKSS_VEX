f43eea94b6e8fe6b6076b30b8018d59c35bcbd9f
Izapicaro <34921842+Izapicaro@users.noreply.github.com>
2018-01-11 18:11:46 +0000

    Update autonomous1.c

--- Revised/autonomous1.c
+++ Revised/autonomous1.c
@@ -1,107 +1,81 @@
-#include "autonomous1.c"
+#pragma systemFile
 
-task main(){
+#include "armLibrary.c"
 
-	//changes value after autonomous
-	int autonomous;
-	autonomous = 1;
+//functions for autonomous
 
-	//set up
-	Robot ROBOT;
-	ROBOT.left = port4;
-	ROBOT.left2 = port5;
-	ROBOT.right = port1;
-	ROBOT.right2 = port2;
-	ROBOT.side = port3;
-	ROBOT.arm1 = port6;
-	ROBOT.arm2 = port7;
-	ROBOT.claw = port8;
-	ROBOT.claw2 = port9;
+//closes claw
+void closeClaw(Robot robot, int seconds){
+	clawFunc(robot, -127);
+	wait1Msec(seconds*1000);
+	endClaw(robot);
+}
 
+//opens claw
+void openClaw(Robot robot, int seconds){
+	clawFunc(robot, 127);
+	wait1Msec(seconds*1000);
+	endClaw(robot);
+}
 
-	//while the autonomous period is ongoing
-	while(autonomous == 1){
+//raises arm
+void raiseArm(Robot robot, int seconds){
+	armFunc(robot, 127);
+	wait1Msec(seconds*1000);
+	endArm(robot);
+}
 
-		if(vexRT[Btn7U]==1){
-			//runs first autonomous strategy
-			auto1(ROBOT);
-			autonomous = 0;
-		}
+//lowers arm
+void lowerArm(Robot robot, int seconds){
+	armFunc(robot, -127);
+	wait1Msec(seconds*1000);
+	endArm(robot);
+}
 
-		else if(vexRT[Btn7D]==1){
-			//runs second autonomous strategy
-			auto2(ROBOT);
-			autonomous = 0;
-		}
+//moves forwards
+void travelForwards(Robot robot, int seconds){
+	goForwards(robot,127,127);
+	wait1Msec(seconds*1000);
+	halt(robot);
+}
 
-		else if(vexRT[Btn7R]==1){
-			//skips both autonomous strategies
-			autonomous = 0;
-		}
+//moves backwards
+void travelBackwards(Robot robot, int seconds){
+	goForwards(robot,-127,-127);
+	wait1Msec(seconds*1000);
+	halt(robot);
+}
 
-	}
+//turns left
+void turnL(Robot robot, int seconds){
+	turn(robot, -127);
+	wait1Msec(seconds*1000);
+	halt(robot);
+}
 
-	//once out of the autonomous period
-	while(autonomous == 0){
+//turns right
+void turnR(Robot robot, int seconds){
+	turn(robot, -127);
+	wait1Msec(seconds*1000);
+	halt(robot);
+}
 
-		//move forwards
-		if(vexRT[Btn8R]==1){
-			goForwards(ROBOT,127,127);
+//autonomous strategy 1
+void auto1(Robot robot){
 
-		}
+	//travel forwards
+	travelForwards(robot,5);
+	//travel backwards
+	travelBackwards(robot,5);
 
-		//move backwards
-		else if(vexRT[Btn8D]==1){
-			goForwards(ROBOT,-127,-127);
-		}
+}
 
-		//move sideways
-		else if(abs(vexRT[Ch4])>0){
-			goSideways(ROBOT,vexRT[Ch4]);
-		}
+//autonomous strategy 2
+void auto2(Robot robot){
 
-		//turn
-		else if(abs(vexRT[Ch1])>0){
-			turn(ROBOT,vexRT[Ch1]);
-		}
-
-		//stop
-		else{
-			halt(ROBOT);
-		}
-
-		//raise arm
-		if(vexRT[Btn5UXmtr2]==1){
-			armFunc(ROBOT,127);
-		}
-
-		//lower arm
-		else if(vexRT[Btn5DXmtr2]==1){
-			armFunc(ROBOT,-127);
-		}
-
-		//switch arm controls to joysticks in case they become out of sync
-		else{
-			motor[ROBOT.arm1] = vexRT[Ch3Xmtr2];
-			motor[ROBOT.arm2] = vexRT[-Ch2Xmtr2];
-		}
-
-		//open claw
-		if(vexRT[Btn6UXmtr2]==1){
-			clawFunc(ROBOT,127);
-		}
-
-		//close claw
-		else if(vexRT[Btn6DXmtr2]==1){
-			clawFunc(ROBOT,-127);
-		}
-
-		//stop claw
-		else {
-			endClaw(ROBOT);
-		}
-
-
-	}
+	//travel forwards
+	travelForwards(robot,3);
+	//travel backwards
+	travelBackwards(robot,3);
 
 }
