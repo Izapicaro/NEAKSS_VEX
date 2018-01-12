f26aafb99b3f4c9bd68e21f34d58fa47a609f579
Izapicaro <34921842+Izapicaro@users.noreply.github.com>
2018-01-12 13:48:06 +0000

    Update armLibrary.c

--- Revised/armLibrary.c
+++ Revised/armLibrary.c
@@ -5,8 +5,10 @@
 
 //arm function
 void armFunc(Robot robot, int armPower){
-	motor[robot.arm1] = armPower;
+	motor[robot.arm1] = -armPower;
 	motor[robot.arm2] = -armPower;
+	motor[robot.arm3] = armPower;
+	motor[robot.arm4] = armPower;
 }
 
 //claw function
@@ -19,6 +21,8 @@ void clawFunc(Robot robot, int clawPower){
 void endArm(Robot robot){
 	motor[robot.arm1] = 0;
 	motor[robot.arm2] = 0;
+	motor[robot.arm3] = 0;
+	motor[robot.arm4] = 0;
 }
 
 //claw stopping function
