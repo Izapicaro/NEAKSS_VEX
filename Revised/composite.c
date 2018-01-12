4a9f0f08dcb3c9b9a3a995723a2e94fafcf7afcc
Izapicaro <34921842+Izapicaro@users.noreply.github.com>
2018-01-12 22:13:36 +0000

    Update composite.c

--- Revised/composite.c
+++ Revised/composite.c
@@ -26,21 +26,21 @@ Robot ROBOT;
 void pre_auton()
 {
 	//construct robot
-	ROBOT.left = port3;
-	ROBOT.left2 = port4;
-	ROBOT.right = port1;
-	ROBOT.right2 = port10;
-	ROBOT.arm1 = port5;	
-	ROBOT.arm2 = port2;
-	ROBOT.arm3 = port6;
-	ROBOT.arm4 = port8;
-	ROBOT.claw = port7;
-	ROBOT.claw2 = port9;
-	
+	ROBOT.left = port2;
+	ROBOT.left2 = port6;
+	ROBOT.right = port10;
+	ROBOT.right2 = port1;
+	ROBOT.arm1 = port3;
+	ROBOT.arm2 = port8;
+	ROBOT.arm3 = port4;
+	ROBOT.arm4 = port9;
+	ROBOT.claw = port5;
+	ROBOT.claw2 = port7;
+
   bStopTasksBetweenModes = false;
 
 	bDisplayCompetitionStatusOnLcd = false;
-	
+
 }
 
 /*---------------------------------------------------------------------------*/
@@ -50,10 +50,10 @@ void pre_auton()
 /*---------------------------------------------------------------------------*/
 
 task autonomous(){
-	
+
 	//run autonomous
 	auto1(ROBOT);
-	
+
 }
 
 /*---------------------------------------------------------------------------*/
@@ -63,64 +63,64 @@ task autonomous(){
 /*---------------------------------------------------------------------------*/
 
 task usercontrol(){
-	
+
 		//once out of the autonomous period
 		while(true){
-	
+
 			//move forwards
 			if(vexRT[Btn8R]==1){
 				goForwards(ROBOT,127,127);
-	
+
 			}
-	
+
 			//move backwards
 			else if(vexRT[Btn8D]==1){
 				goForwards(ROBOT,-127,-127);
 			}
-	
+
 			//turn
 			else if(abs(vexRT[Ch1])>0){
 				turn(ROBOT,vexRT[Ch1]);
 			}
-	
+
 			//stop
 			else{
 				halt(ROBOT);
 			}
-	
+
 			//raise arm
 			if(vexRT[Btn5UXmtr2]==1){
 				armFunc(ROBOT,127);
 			}
-	
+
 			//lower arm
 			else if(vexRT[Btn5DXmtr2]==1){
 				armFunc(ROBOT,-127);
 			}
-	
+
 			//switch arm controls to joysticks in case they become out of sync
 			else{
 				motor[ROBOT.arm1] = -vexRT[Ch3Xmtr2];
-				motor[ROBOT.arm2] = -vexRT[Ch3Xmtr2];
-				motor[ROBOT.arm3] = vexRT[Ch2Xmtr2];
+				motor[ROBOT.arm2] = vexRT[Ch3Xmtr2];
+				motor[ROBOT.arm3] = -vexRT[Ch2Xmtr2];
 				motor[ROBOT.arm4] = vexRT[Ch2Xmtr2];
 			}
-	
+
 			//open claw
 			if(vexRT[Btn6UXmtr2]==1){
 				clawFunc(ROBOT,127);
 			}
-	
+
 			//close claw
 			else if(vexRT[Btn6DXmtr2]==1){
 				clawFunc(ROBOT,-127);
 			}
-	
+
 			//stop claw
 			else {
 				endClaw(ROBOT);
 			}
-			
+
 		}
-		
+
 }
